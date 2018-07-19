2c45934d45315ddef5471630e42e792011c9caba
Kevin Alexander Argueta <kevin_argueta@alumni.brown.edu>
2018-07-19 05:52:24 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,7 @@
-#Default is World
-#Author : Kevin Argueta
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
